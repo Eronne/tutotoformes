6801628b5ef39fc3ddc73a1a3e532d66de66214c
Omar Jbara <omar.jbara2@gmail.com>
2016-08-29 20:01:47 +0200

    Changed ID strategy

--- src/AppBundle/Entity/Role.php
+++ src/AppBundle/Entity/Role.php
@@ -18,7 +18,7 @@ class Role implements RoleInterface
      *
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $id;
 
