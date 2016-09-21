656ce898c82df3122048d2920d88c867fe406a91
Omar Jbara <omar.Jbara2@gmail.com>
2016-09-21 16:29:49 +0200

    Updated style

--- web/js/app.js
+++ web/js/app.js
@@ -5,17 +5,18 @@ $(document).ready(function () {
     ;
     $('.ui.rating')
         .rating({
-            onRate: function(value) {
+            onRate: function (value) {
                 $('input[name="_tutoriel[difficulty]"]').val(value);
             }
         })
     ;
     $('.message .close')
-        .on('click', function() {
+        .on('click', function () {
             $(this)
                 .closest('.message')
                 .transition('fade')
             ;
         })
     ;
+
 });
